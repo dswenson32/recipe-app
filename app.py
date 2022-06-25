f4406a91b7c0940a38034cffa32f7564ee9058fc
devinswenson <devin.swenson@affirm.com>
2022-06-24 21:31:11 -0500

    Refactoring the app.py. Moving some logic into their own separate function instead of calling inside the route functions themselves. Creating a JSON object that will act as the "in memory" database for all recipes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import yaml
 
 app = Flask(__name__)
 
+main_recipe_data = {}
 
 @app.route('/oldtest')
 def hello_world(recipe=None):
@@ -21,12 +22,38 @@ def hello_world(recipe=None):
     }
     return render_template('recipe.html', json_data=json_data)
 
-
+# WEB PAGE ROUTES - START
 @app.route('/create')
 def create():
     return render_template('create.html')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+# WEBPAGE ROUTES - END
+
+
+# SERVICE ROUTES - START
 @app.route('/getRecipes')
+def call_get_recipes():
+    return get_recipes()
+
+@app.route('/submit-recipe', methods=['POST'])
+def submit_recipe():
+    print(request.form)
+    build_recipe(request.form)
+    print("Well we're here... we called the endpoint")
+    return ""
+# SERVICE ROUTES - END
+
+# FUNCTIONS - START
+def build_recipe(args):
+    # Define Title and Servings
+    recipe = {"Title": args.get("title"), "Servings": args.get("servings")}
+    with open('/Users/devinswenson/PycharmProjects/recipe_book/static/test_yaml.yml', 'w') as file:
+        yaml.dump(recipe, file)
+    print(recipe)
+
 def get_recipes(recipes=[]):
     with open('/Users/devinswenson/PycharmProjects/recipe_book/static/test_yaml.yml', 'r') as file:
         data = yaml.safe_load_all(file)
@@ -37,11 +64,6 @@ def get_recipes(recipes=[]):
                 "Servings": recipe.get('Servings')
             })
     return jsonify(recipes)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
