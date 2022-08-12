ea4cf70fab3890d26474b52ffd3d8482c499415f
devinswenson <devin.swenson@affirm.com>
2022-08-12 16:56:54 -0500

    Add edit route Cleaned up some route ordering and added a new button to the datatable

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 import yaml
 
 app = Flask(__name__)
+
 
 @app.route('/test-save')
 def test_save():
@@ -9,83 +10,76 @@ def test_save():
     save_recipes(recipes)
     return "It's done"
 
-@app.route('/recipe/<name>')
-def get_recipe(name):
-    with open('/Users/devinswenson/PycharmProjects/recipe_book_new/static/test_yaml.yml', 'r') as file:
-        data = yaml.safe_load_all(file)
-        for doc in data:
-            if doc.get('Title') == name:
-                recipe = doc
-                print(doc)
-    json_data = {
-        "Title": recipe.get('Title'),
-        "Ingredients": recipe.get('Ingredients'),
-        "Instructions": recipe.get('Instructions'),
-        "Servings": recipe.get('Servings')
-    }
-    return render_template('recipe.html', json_data=json_data)
 
 # WEB PAGE ROUTES - START
-@app.route('/create')
-def create():
-    return render_template('create.html')
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def get_recipe(name):
-    with open('/Users/devinswenson/PycharmProjects/recipe_book_new/static/test_yaml.yml', 'r') as file:
-        data = yaml.safe_load_all(file)
-        for doc in data:
-            if doc.get('Title') == name:
-                recipe = doc
-                print(doc)
-    json_data = {
-        "Title": recipe.get('Title'),
-        "Ingredients": recipe.get('Ingredients'),
-        "Instructions": recipe.get('Instructions'),
-        "Servings": recipe.get('Servings')
-    }
-    return render_template('recipe.html', json_data=json_data)
-# WEBPAGE ROUTES - END
 
+@app.route('/recipe/<name>')
+def get_recipe(name):
+    return render_template('recipe.html', json_data=get_recipe(name))
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
+@app.route('/edit/<name>')
+def edit(name):
+    return render_template('edit.html', json_data=get_recipe(name))
+
+
+# WEBPAGE ROUTES - END
 
 # SERVICE ROUTES - START
 @app.route('/getRecipes')
 def call_get_recipes():
     return get_recipes()
 
-@app.route('/submit-recipe', methods=['POST'])
+
+@app.route('/submit-recipe', methods=['POST', 'PUT'])
 def submit_recipe():
-    print(request.form)
     built_recipe = build_recipe(request.form)
-    print("Well we're here... we called the endpoint")
     recipes = get_recipes().json
+    if request.method == 'PUT':
+        for recipe in recipes:
+            if recipe.get("Title") == built_recipe.get("Title"):
+                print("i found it!")
+                recipes.remove(recipe)
+                break
     recipes.append(built_recipe)
     save_recipes(recipes)
-    return ""
+    return redirect('/')
+
+
 # SERVICE ROUTES - END
 
 # FUNCTIONS - START
 def build_recipe(args):
-    recipe = {"Title": str(args.get("title")), "Servings": str(args.get("servings")), "Ingredients": [], "Instructions": []}
+    recipe = {"Title": str(args.get("title")), "Servings": str(args.get("servings")), "Ingredients": [],
+              "Instructions": []}
     idx = 1
     while True:
         if str(args.get("quantity-" + str(idx))) == "None":
             break
-        recipe["Ingredients"].append([str(args.get("quantity-" + str(idx))), str(args.get("measure-" + str(idx))), str(args.get("ingredient-" + str(idx)))])
-        idx = idx+1
+        recipe["Ingredients"].append([str(args.get("quantity-" + str(idx))), str(args.get("measure-" + str(idx))),
+                                      str(args.get("ingredient-" + str(idx)))])
+        idx = idx + 1
     idx = 1
     while True:
         if str(args.get("direction-" + str(idx))) == "None":
             break
         recipe["Instructions"].append(str(args.get("direction-" + str(idx))))
-        idx = idx+1
+        idx = idx + 1
     print(recipe)
     return recipe
 
-def get_recipes(recipes=[]):
+
+def get_recipes():
+    recipes = []
     with open('/Users/devinswenson/PycharmProjects/recipe_book_new/static/test_yaml.yml', 'r') as file:
         data = yaml.safe_load_all(file)
         for recipe in data:
@@ -100,6 +94,23 @@ def get_recipes(recipes=[]):
                 "Instructions": recipe.get("Instructions")
             })
     return jsonify(recipes)
+
+
+def get_recipe(name):
+    with open('/Users/devinswenson/PycharmProjects/recipe_book_new/static/test_yaml.yml', 'r') as file:
+        data = yaml.safe_load_all(file)
+        for doc in data:
+            if doc.get('Title') == name:
+                recipe = doc
+                print(doc)
+    json_data = {
+        "Title": recipe.get('Title'),
+        "Ingredients": recipe.get('Ingredients'),
+        "Instructions": recipe.get('Instructions'),
+        "Servings": recipe.get('Servings')
+    }
+    return json_data
+
 
 def save_recipes(recipes):
     with open('/Users/devinswenson/PycharmProjects/recipe_book_new/static/test_yaml.yml', 'w') as file:
